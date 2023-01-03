a839d78595856b9ae43fb60216efcc235c749c59
hamza.mughal5593 <hamza.mughal5593@gmail.com>
2023-01-03 19:01:29 +0500

    03-01-23

--- app/src/main/java/com/turbo/app/turbovpnpro/UI/login/ForgetPasswordActivity.java
+++ app/src/main/java/com/turbo/app/turbovpnpro/UI/login/ForgetPasswordActivity.java
@@ -3,6 +3,7 @@ package com.turbo.app.turbovpnpro.UI.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.turbo.app.turbovpnpro.R;
 
@@ -12,5 +13,18 @@ public class ForgetPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        findViewById(R.id.back_logins).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        findViewById(R.id.back_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
