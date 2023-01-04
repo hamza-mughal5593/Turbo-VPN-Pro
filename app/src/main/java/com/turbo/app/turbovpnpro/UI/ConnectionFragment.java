77fb80ab85bcfb0ea32906c7a274d25c9d213fc8
hamzanawaz <hamza.mughal5593@gmail.com>
2023-01-05 02:00:02 +0500

    4 jan

--- app/src/main/java/com/turbo/app/turbovpnpro/UI/ConnectionFragment.java
+++ app/src/main/java/com/turbo/app/turbovpnpro/UI/ConnectionFragment.java
@@ -18,5 +18,6 @@ public class ConnectionFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_connection, container, false);
+
     }
 }
